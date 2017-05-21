97a7568535ca4849a0a825e5caeada7319495b0f
bobianikolovb <bobianikolov@gmail.com>
2017-05-22 02:04:45 +0300

    added tasks

---
+++ Programming Fundamentals-C#/1.GitGitHubDebuggingSearhingLab/01WeekendsBetweenTwoDates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01WeekendsBetweenTwoDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            int holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if ((date.DayOfWeek == DayOfWeek.Saturday) || (date.DayOfWeek == DayOfWeek.Sunday)) 
+                {
+                    holidaysCount++;
+                }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
