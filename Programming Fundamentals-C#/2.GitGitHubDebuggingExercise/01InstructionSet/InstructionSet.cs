70cd1fa4be365735ffd9703ef2ed0e3c04400a0b
bobianikolovb <bobianikolov@gmail.com>
2017-05-22 02:08:28 +0300

    added problems

---
+++ Programming Fundamentals-C#/2.GitGitHubDebuggingExercise/01InstructionSet/InstructionSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+
+        string opCode = Console.ReadLine();
+
+        while (opCode != "END")
+        {
+            string[] codeArgs = opCode.Split(' ');
+
+            long result = 0;
+            switch (codeArgs[0])
+            {
+                case "INC":
+                    {
+                        long operandOne = long.Parse(codeArgs[1]);
+                        result = operandOne + 1; break;
+                    }
+                case "DEC":
+                    {
+                        long operandTwo = long.Parse(codeArgs[1]);
+                        result = operandTwo - 1; ; break;
+                    }
+                case "ADD":
+                    {
+                        long operandThree = long.Parse(codeArgs[1]);
+                        long operandFour = long.Parse(codeArgs[2]);
+                        result = operandThree + operandFour; break;
+                    }
+                case "MLA":
+                    {
+                        long operandFive = long.Parse(codeArgs[1]);
+                        long operandSix = long.Parse(codeArgs[2]);
+                        result = (long)(operandFive * operandSix); break;
+                    }
+            }
+            opCode = Console.ReadLine();
+            Console.WriteLine(result);
+        }
+    }
+}
