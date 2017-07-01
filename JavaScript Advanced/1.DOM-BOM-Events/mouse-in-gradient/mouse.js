b3c0b78430749ce59bf7296bbc7b24d5720a59aa
bobianikolovb <bobianikolov@gmail.com>
2017-07-01 12:19:18 +0300

    added mouse in gradient problem

---
+++ JavaScript Advanced/1.DOM-BOM-Events/mouse-in-gradient/mouse.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Bobi on 07/01/2017.
+ */
+$(document).ready(function () {
+    "use strict";
+
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + '%';
+    }
+
+    function gradientOut(event) {
+        document.getElementById('result').textContent = '';
+    }
+});
